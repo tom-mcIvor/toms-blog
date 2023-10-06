f24f075ba6dd68eb3cd670683769c881dc22aa5a
tommy-tommy-tom <105212576+tom-mcivor@users.noreply.github.com>
2023-10-07 00:15:06 +1300

    add styling to the html

--- src/app/nav/page.tsx
+++ src/app/nav/page.tsx
@@ -1,49 +1,78 @@
 import React from 'react'
-import { makeStyles } from '@material-ui/core/styles';
-import FormControl from '@material-ui/core/FormControl';
-import NativeSelect from '@material-ui/core/NativeSelect';
-
-const useStyles = makeStyles((theme) => ({
-  formControl: {
-    margin: theme.spacing(1),
-    minWidth: 120,
-  },
-  selectEmpty: {
-    marginTop: theme.spacing(2),
-  },
-}));
+import Image from 'next/image';
+import Link from 'next/link'
+import profilePic from './tom.png'
 
 function Nav() {
-  const classes = useStyles();
-  const [state, setState] = React.useState({
-    age: '',
-    name: 'hai',
-  });
+ 
 
 
-  const handleChange = (event) => {
-    const name = event.target.name;
-    setState({
-      ...state,
-      [name]: event.target.value,
-    });
-  };
 
   return (
-     <FormControl className={classes.formControl}>
-        <NativeSelect
-          value={state.age}
-          onChange={handleChange}
-          name="age"
-          className={classes.selectEmpty}
-          inputProps={{ 'aria-label': 'age' }}
-        >
-          <option value="">None</option>
-          <option value={10}>Ten</option>
-          <option value={20}>Twenty</option>
-          <option value={30}>Thirty</option>
-        </NativeSelect>
-      </FormControl>   
+      <>
+      <div className="navbar active">
+      <div className="left-part">
+      <Link href="https://github.com/tom-mcivor">
+          <div className="small">Toms Blog</div>
+          <Image 
+            src={profilePic}
+            alt="toms profile"
+          />
+      </Link>
+    </div>
+
+      
+        {/* <a className="change" href="../index.html">
+          <div className="container50">
+            <div className="center">
+              <button className="btn">
+                <div className=" hello">Home</div>
+                <svg width="180px" height="60px" viewBox="0 0 180 60" className="border">
+                  <polyline points="179,1 179,59 1,59 1,1 179,1" className="bg-line" />
+                  <polyline points="179,1 179,59 1,59 1,1 179,1" className="hl-line" />
+                </svg>
+              </button>
+            </div>
+          </div>
+        </a> */}
+      
+    
+    <nav className="hello10">
+      <ul>
+        <li><a href="#"></a>
+          <ul>
+            <li><a href="html-css.html">CSS lessons</a></li>
+            <li><a href="javascript-dom.html">Javascript DOM</a></li>
+            <li><a href="problem-solving.html">Problem Solving</a></li>
+          </ul>
+        </li>
+        <li><a href="#"></a>
+          <ul>
+            <li><a href="neuroplasticity.html">Neuroplasticity ideas</a></li>
+            <li><a href="emotional-intelligence.html">Emotional IQ</a></li>
+            <li><a href="identity-values.html">Identity values</a></li>
+            <li><a href="foundations-reflection.html">Foundations reflection</a></li>
+            <li><a href="learning-plan.html">Learning plan</a></li>
+          </ul>
+        </li>
+      </ul>
+    </nav>
+    <ul className="this-one">
+      <div className="tech">
+        <li><a href="html-css.html">CSS lessons</a></li>
+        <li><a href="javascript-dom.html">Javascript DOM</a></li>
+        <li><a href="problem-solving.html">Problem solving</a></li>
+      </div>
+      <div className="core">
+        <li><a href="neuroplasticity.html">Neuroplasticity ideas</a></li>
+        <li><a href="emotional-intelligence.html">Emotional IQ</a></li>
+        <li><a className="change-3" href="identity-values.html">Identity values</a></li>
+        <li><a href="foundations-reflection.html">Foundations reflection</a></li>
+        <li><a href="learning-plan.html">Learning plan</a></li>
+      </div>
+    </ul>
+  </div>
+      </>
   )
 }
 
