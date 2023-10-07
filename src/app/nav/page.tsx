e9b003c884c2c00a204765479fcbff4d2b121641
tommy-tommy-tom <105212576+tom-mcivor@users.noreply.github.com>
2023-10-08 00:21:21 +1300

    add styling to the html

--- src/app/nav/page.tsx
+++ src/app/nav/page.tsx
@@ -4,29 +4,43 @@ import React from 'react'
 import Image from 'next/image';
 import Link from 'next/link'
 import profilePic from '../../../img/tom.png'
+import { useState } from 'react';
 import {
   Menu,
   MenuHandler,
   MenuList,
   MenuItem,
   Button,
-  ThemeProvider,
+  Card,
+  Typography,
 } from "@material-tailwind/react";
+import { ChevronDownIcon, RocketLaunchIcon } from "@heroicons/react/24/outline";
+
+
+const menuItems = [
+  {
+    title: "@material-tailwind/html",
+    description:
+      "Learn how to use @material-tailwind/html, packed with rich components and widgets.",
+  },
+  {
+    title: "@material-tailwind/react",
+    description:
+      "Learn how to use @material-tailwind/react, packed with rich components for React.",
+  },
+  {
+    title: "Material Tailwind PRO",
+    description:
+      "A complete set of UI Elements for building faster websites in less time.",
+  },
+];
+
 
 function Nav() {
-  const theme = {
-    menu: {
-      styles: {
-        base: { 
-          menu: {
-            zIndex: "z-[1000]",
-          },
-        },
-      },
-    },
-  }
+
+  const [openMenu, setOpenMenu] = React.useState(false)
+
   return (
-    <ThemeProvider value={theme}>
       <div className="navbar active">
         <div className="left-part">
           <Link href="https://github.com/tom-mcivor">
@@ -58,28 +72,52 @@ function Nav() {
             </div>
           </a>
         </div>
-        <div className="menu">
-          <Menu>
-            <MenuHandler>
-              <Button>Tech &or;</Button>
-            </MenuHandler>
-            <MenuList>
-              <MenuItem>Menu Item 1</MenuItem>
-              <MenuItem>Menu Item 2</MenuItem>
-              <Menu placement="right-start" offset={15}>
-                <MenuHandler>
-                  <MenuItem>Nested Item</MenuItem>
-                </MenuHandler>
-                <MenuList>
-                  <MenuItem>Nested Item 1</MenuItem>
-                  <MenuItem>Nested Item 2</MenuItem>
-                  <MenuItem>Nested Item 3</MenuItem>
-                </MenuList>
-              </Menu>
-              <MenuItem>Menu Item 3</MenuItem>
-            </MenuList>
-          </Menu>
-        </div>
+
+        <Menu open={openMenu} handler={setOpenMenu} allowHover>
+      <MenuHandler>
+        <Button
+          variant="text"
+          className="flex items-center gap-3 text-base font-normal capitalize tracking-normal"
+        >
+          Technology{" "}
+          <ChevronDownIcon
+            strokeWidth={2.5}
+            className={`h-3.5 w-3.5 transition-transform ${
+              openMenu ? "rotate-180" : ""
+            }`}
+          />
+        </Button>
+      </MenuHandler>
+      <MenuList className="hidden w-[36rem] grid-cols-7 gap-3 overflow-visible lg:grid">
+        <Card
+          color="gray"
+          shadow={false}
+          variant="gradient"
+          className="col-span-3 grid h-full w-full place-items-center rounded-md"
+        >
+          <RocketLaunchIcon strokeWidth={1} className="h-28 w-28" />
+        </Card>
+        <ul className="col-span-4 flex w-full flex-col gap-1">
+          {menuItems.map(({ title, description }) => (
+            <a href="#" key={title}>
+              <MenuItem>
+                <Typography variant="h6" color="blue-gray" className="mb-1">
+                  {title}
+                </Typography>
+                <Typography
+                  variant="small"
+                  color="gray"
+                  className="font-normal"
+                >
+                  {description}
+                </Typography>
+              </MenuItem>
+            </a>
+          ))}
+        </ul>
+      </MenuList>
+    </Menu>
+
         <nav className="hello10">
           <ul>
             <li>
@@ -153,7 +191,6 @@ function Nav() {
           </div>
         </ul>
       </div>
-    </ThemeProvider>
   )
 }
 
