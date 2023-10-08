b31619e20447043ede4396ea5c0e0919074fa73e
tommy-tommy-tom <105212576+tom-mcivor@users.noreply.github.com>
2023-10-08 23:14:53 +1300

    add styling to the html

--- src/app/nav/page.tsx
+++ src/app/nav/page.tsx
@@ -94,14 +94,7 @@ function Nav() {
     <ThemeProvider value={theme}>
         <div className="navbar active">
       <div className="flex items-center justify-between text-blue-gray-900">
-        <Typography
-          as="a"
-          href="#"
-          variant="h6"
-          className="mr-4 cursor-pointer py-1.5"
-        >
-          Material Tailwind
-        </Typography>
+
         <div className="hidden lg:block">
           <NavList />
         </div>
